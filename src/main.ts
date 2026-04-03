97dd0eeec716020e3c57ff5639ff3a504b32ade0
madeOliva <161033518+madeOliva@users.noreply.github.com>
2026-04-03 09:35:15 -0400

    add changes in kardex, producto

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs",app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 
   
 
--- src/modules/producto/schemas/producto.schema.ts
+++ src/modules/producto/schemas/producto.schema.ts
@@ -4,31 +4,43 @@ import { HydratedDocument } from "mongoose";
 
 export type ProductoDocument = HydratedDocument<Producto>;
 
+export enum ProductoEstado {
+    ACTIVO = 'activo',
+    INACTIVO = 'inactivo',
+}
+
+export enum ProductoCategoria {
+    TECNOLOGÍAS = 'tecnologías',
+    ELECTRODOMÉSTICOS = 'electrodomésticos',
+    ACCESORIOS = 'accesorios',
+    OFICINA = 'oficina',
+}
+
 @Schema()
 export class Producto {
-    @Prop({required:true, unique: true })
+    @Prop({ required: true, unique: true })
     codigo_producto: string;
 
-    @Prop({ required: true})
+    @Prop({ required: true })
     nombre_producto: string;
 
-    @Prop({required:true})
-    categoria_producto: string;
+    @Prop({ required: true, enum:ProductoCategoria })
+    categoria_producto: ProductoCategoria;
 
-    @Prop({required:true})
-    precio_compra:number;
+    @Prop({ required: true })
+    precio_compra: number;
 
-    @Prop({required:true})
-    precio_venta:number;
+    @Prop({ required: true })
+    precio_venta: number;
 
-    @Prop({required:true})
-    stock_inicial:number; //cantidad inicial
+    @Prop({ required: true })
+    stock_inicial: number; //cantidad inicial
 
-    @Prop({required:true})
-    stock_minimo:number; // cantidad minima que debe haber
+    @Prop({ required: true })
+    stock_minimo: number; // cantidad minima que debe haber
 
-    @Prop({required:true})
-    estado: string;
+    @Prop({ required: true, enum: ProductoEstado })
+    estado: ProductoEstado;
 
 
 }
