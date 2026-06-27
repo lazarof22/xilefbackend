2160787dd969a9e01c95a811058f004f6887e4f0
Madelaine <madelainepadilla3@gmail.com>
2026-06-27 18:36:53 -0400

    feat: implementar módulos Almacen y Contenedor
    - Crear esquemas Mongoose para Almacen y Contenedor
    - Agregar DTOs con validaciones class-validator
    - Implementar servicios con CRUD completo
    - Validar existencia de almacén al crear contenedor
    - Sincronizar cantidadContenedores en almacén automáticamente
    -documentación con swagger
    -en los contenedores implemente un metodo de filtrado por almacenes

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,6 +31,8 @@ import { ImportExportModule } from './modules/configuracion/import-export/import
 import { ReportePlusModule } from './modules/venta/reporte_plus/reporte_plus.module';
 import { ReporteCajaModule } from './modules/venta/reporte_caja/reporte_caja.module';
 import { ConteoFisicoModule } from './modules/contabilidad/conteo_fisico/conteo_fisico.module';
+import { ContenedorModule } from './modules/inventario/contenedor/contenedor.module';
+import { AlmacenModule } from './modules/inventario/almacen/almacen.module';
 
 
 @Module({
@@ -47,12 +49,14 @@ import { ConteoFisicoModule } from './modules/contabilidad/conteo_fisico/conteo_
     }),
     //Iniciacion de los modulos
     ActivoFijoModule,
+    AlmacenModule,
     AreaModule,
     AuthModule,
     CategoriaModule,
     CargoEmpleadoModule,
     ClienteModule,
     ConceptoModule,
+    ContenedorModule,
     CompraModule,
     CuentaModule,
     DepartamentoModule,
